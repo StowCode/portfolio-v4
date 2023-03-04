80a7de5e9aa1f66f540df0b4390d94b9761e5e8e
Mitchell Stowman <mitchellstowman@gmail.com>
2023-03-04 18:03:19 -0500

    Working and styled navbar implemented - Added static files from previous site - Added google fonts from previous site

--- src/routes/Navigation/navigation.routes.jsx
+++ src/routes/Navigation/navigation.routes.jsx
@@ -1,15 +1,26 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
+import '../Navigation/navigation.styles.scss';
+
 const Navigation = () => {
     return(
         <Fragment>
-            <ul>
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/projects'>Projects</Link>
-                <Link to='/contact'>Contact</Link>
-            </ul>
+
+            <div className='navbar'>
+                <Link to='/'>
+                    <img className='nav-logo' src='https://ucarecdn.com/aac08051-ae26-4543-b75b-857732aaeb05/-/preview/-/quality/smart/'/>
+                </Link>
+                <ul className='navlinks'>
+                    <Link className='link' to='/'>Home</Link>
+                    <Link className='link' to='/about'>About</Link>
+                    <Link className='link' to='/projects'>Projects</Link>
+                    <Link className='link' to='/contact'>Contact</Link>
+                    <span className='resume-btn link'>Resum&eacute;</span>
+                </ul>
+                
+            </div>
+
         <Outlet />
         </Fragment>
     )
