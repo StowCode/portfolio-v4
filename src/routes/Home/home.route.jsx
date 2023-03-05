0dbc10dd84b42d55e98c2fb98737914e6217737d
Mitchell Stowman <mitchellstowman@gmail.com>
2023-03-05 11:36:31 -0500

    Created Sidebar component - Imported SVGs as components - Set fill value to 'current' to allow scss styling - Added Sidebar to Home

--- src/routes/Home/home.route.jsx
+++ src/routes/Home/home.route.jsx
@@ -1,7 +1,26 @@
+// Comps
+import SideBar from '../../comps/sidebar/sidebar.comp.';
+
+// Style
+import '../Home/home.styles.scss';
+
 const Home = () => {
     return(
-        <div>
-            <h1>Home</h1>
+        <div className='page home-page'>
+
+            <div className='flex-container'>
+                <div className='hero-text'>
+                    <h3>Welcome to the portfolio of</h3>
+                    <h1>Mitchell Stowman</h1>
+                    <h3>Frontend Developer</h3>
+                </div>
+
+                <div id='canvas'>
+
+                </div>
+            </div>
+
+            <SideBar />
         </div>
     )
 }
